bbeb1e642f1bdace8c93b045ada31f7961746fc5
rafaelsnavarro <rafaelsnavarro26@gmail.com>
2022-08-13 14:29:45 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+let comedy = [
+  {
+    title: "The wite chicks",
+    year: 2004,
+    director: "Brothers Wayans"
+  }
+];
+let action = [
+  {
+    title: "Mars",
+    year: 2015,
+    director: "Ridley Scott"
+  }
+];
+let fantasy = [
+  {
+    title: "The Wizard of Oz",
+    year: 1939,
+    director: "Victor Fleming"
+  }
+];
+
+let memory = true;
+if (memory) {
+  console.log("My favorite movie is The Wite chicks");
+} else {
+  console.log("Not even five stars");
+}
